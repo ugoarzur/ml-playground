d0626f9e4727b421fe80daccc7b7e6327a84259d
Ugo Arzur <ugo.arzur@fabernovel.ey.com>
2026-03-17 14:55:03 +0100

    chore: update default house features values

--- backend/src/houses/schemas.py
+++ backend/src/houses/schemas.py
@@ -1,12 +1,12 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class HouseFeatures(BaseModel):
-    MedInc: float
-    HouseAge: float
-    AveRooms: float
-    AveBedrms: float
-    Population: float
-    AveOccup: float
-    Latitude: float
-    Longitude: float
+    MedInc: float = Field(examples=[8.3252])
+    HouseAge: float = Field(examples=[41.0])
+    AveRooms: float = Field(examples=[6.98])
+    AveBedrms: float = Field(examples=[1.02])
+    Population: float = Field(examples=[322.0])
+    AveOccup: float = Field(examples=[2.55])
+    Latitude: float = Field(examples=[37.88])
+    Longitude: float = Field(examples=[-122.23])
